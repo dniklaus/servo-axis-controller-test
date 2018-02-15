81a1f55efe849b7bb3d7d7fda705a447a55f5598
Dieter Niklaus <dieter.niklaus@gmx.net>
2018-02-15 09:16:16 +0100

    add TargetReachedNotifier

--- src/servo-axis-controller-test.cpp
+++ src/servo-axis-controller-test.cpp
@@ -37,25 +37,54 @@ int servoPin = 2;
 class ServoAxis : public IServoHal
 {
   Servo* m_servo;
+
 public:
   ServoAxis(int servoPin)
   : m_servo(new Servo())
   {
     m_servo->attach(servoPin);
   }
+
   void setAngle(int angle)
   {
-    m_servo->write(angle);
+    m_servo->write(map(angle, -90, 90, 0, 180));
+  }
+};
+
+class TargetReachedNotifier : public ITargetReachedNotifier
+{
+  Axis* m_axis;
+  DbgTrace_Port* m_trPort;
+  char* m_trPortName;
+
+public:
+  TargetReachedNotifier(Axis* axis)
+  : m_axis(axis)
+  , m_trPort(0)
+  , m_trPortName(new char[strlen(m_axis->name())+2])
+  {
+    memset(m_trPortName, 0, strlen(m_axis->name())+2);
+    sprintf(m_trPortName, "t%s", m_axis->name());
+    m_trPort = new DbgTrace_Port(m_trPortName, DbgTrace_Level::debug);
+  }
+
+  void notifyTargetReached(int targetAngle)
+  {
+    TR_PRINTF(m_trPort, DbgTrace_Level::debug, "Target reached (%d)", targetAngle);
   }
 };
 
 class DbgCmd_SetAngle : public DbgCli_Command
 {
+private:
   Axis* m_axis;
+  DbgTrace_Port* m_trPort;
+  
 public:
   DbgCmd_SetAngle(Axis* axis)
-  : DbgCli_Command(new DbgCli_Topic(DbgCli_Node::RootNode(), axis->name(), "Axis debug commands"), "set", "Set angle [-90°..90°].")
+  : DbgCli_Command(new DbgCli_Topic(DbgCli_Node::RootNode(), axis->name(), "Axis debug commands"), "set", "Set angle {-90..90} with defined velocity {1..40}.")
   , m_axis(axis)
+  , m_trPort(new DbgTrace_Port(m_axis->name(), DbgTrace_Level::debug))
   { }
 
   void execute(unsigned int argc, const char** args, unsigned int idxToFirstArgToHandle)
@@ -71,13 +100,7 @@ public:
       angle = atoi(args[idxToFirstArgToHandle]);
       velocity = atoi(args[idxToFirstArgToHandle+1]);
       m_axis->goToTargetAngle(angle, velocity);
-      Serial.println();
-      Serial.print("Axis set angle: ");
-      Serial.print(angle);
-      Serial.print(" [*]");
-      Serial.print(", velocity: ");
-      Serial.print(velocity);
-      Serial.println(" [*/ms]");
+      TR_PRINTF(m_trPort, DbgTrace_Level::debug, "Moving from %d to %d, velocity: %d", m_axis->getAngle(), angle, velocity);
     }
   }
 
@@ -85,7 +108,7 @@ public:
   {
     Serial.println(getHelpText());
     Serial.println("Usage: dbg axis set <angle> <velocity>");
-    Serial.println("       angle: -90..90 [*], velocity: 1..40");
+    Serial.println("       angle: -90..90, velocity: 1..40");
   }
 };
 
@@ -96,14 +119,15 @@ void setup()
 
   setupProdDebugEnv();
 
-  char axisName[5];
-  memset(axisName, 0, strlen(axisName+1));
-
+  char* axisName;
   for (unsigned int i = 0; i < 8; i++)
   {
+    axisName = new char[5];
+    memset(axisName, 0, strlen(axisName)+1);
     sprintf(axisName, "ax%d", i);
     axis = new Axis(axisName);
     axis->attachServoHal(new ServoAxis(servoPin+i));
+    axis->attachTargetReachedNotifier(new TargetReachedNotifier(axis));
     new DbgCmd_SetAngle(axis);
   }
 }
